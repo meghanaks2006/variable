990614344098230e3972c41d9648dcc976b0eced
Meghana <meghanaks2006@gmail.com>
2026-07-10 21:07:15 +0530

    Add DataType program

---
+++ DataType.java
@@ -0,0 +1,21 @@
+class DataType 
+{
+    public static void main(String a[])
+    {
+        int num1 = 9;
+        byte by = 127;
+        short sh =384;
+        long l = 2847l;
+
+        float f = 3.5f;
+        double d = 4.4;
+        char c = 'k';
+        boolean b = true;
+        System .out.println("int="+num1);
+        System .out.println("byte="+by);
+        System .out.println("short="+sh);
+        System .out.println("long="+l);
+        System .out.println("double="+d);
+        System .out.println("char="+c);
+    }
+}
