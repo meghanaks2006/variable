64e8564271c9c8125fa6b88c66b1685feb0b30ed
Meghana <meghanaks2006@gmail.com>
2026-07-14 21:12:09 +0530

    Added conversion.java

---
+++ conversion.java
@@ -0,0 +1,11 @@
+class conversion
+{
+    public static void main(String []args)
+    {
+        byte b = 127;
+        int a = 276;
+        byte k = (byte) a;
+        System.out.println(k);
+
+    }
+}
