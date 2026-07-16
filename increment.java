612de75818c2a26c2f4b5d1c57bb4ecee1ea8f5d
Meghana <meghanaks2006@gmail.com>
2026-07-16 19:51:55 +0530

    Added relational and increment program

---
+++ increment.java
@@ -0,0 +1,13 @@
+class increment
+{
+    public static void main (String []args)
+    {
+    int num = 7;
+    num +=2;
+    // num -= 2; output =5
+    //num =num+3; output = 10
+    //++num; output = 8
+    //num++; output =8
+    System.out.println(num);
+    }
+}
---
+++ relational.java
@@ -0,0 +1,15 @@
+class relational 
+{
+    public static void main (String args[]){
+        int num1 =7;
+        int num2 =7;
+        //int num2 =8;
+        //boolean result = num1>num2;
+        //boolean result = num1<num2;
+        //boolean result =num1>=num2;
+        //boolean result = num1<=num2;
+        //boolean result = num1!=num2;
+        boolean result = num1==num2;
+        System.out.println(result);
+    }
+}
