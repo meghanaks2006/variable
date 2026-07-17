716a19109322cfd125d59fab73fcb81cdd8fff5c
Meghana <meghanaks2006@gmail.com>
2026-07-17 22:23:23 +0530

    Added ifelser program

---
+++ if-else.java
@@ -0,0 +1,15 @@
+class ifelse
+{
+    public static void main(String args[])
+    {
+        int a = 5;
+        
+        if (a>0) {
+        
+        System.out.println("number is positive");
+        }
+        else{
+            System.out.println("negative");
+        }
+    }
+}
