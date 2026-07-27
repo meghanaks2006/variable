8e15e70e86bad869fba76f0160b8946ca5fecec6
Meghana <meghanaks2006@gmail.com>
2026-07-27 21:14:56 +0530

    Added ternary program

--- ifelseif.java
+++ ifelseif.java
@@ -1,1 +1,12 @@
-class ifelseif
+public class ifelseif{
+    public static void main(String args[]){
+        String name = "meghana";
+        if (name =="ananya"){
+                System.out.println("name is"+name);
+        }else if (name == "hima"){
+                System.out.println("name is"+name);
+        }else{
+                System.out.println(name);
+        }
+    }
+}
