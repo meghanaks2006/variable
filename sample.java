81d74ad2e329a85cb502762158abf158c451222f
Meghana <meghanaks2006@gmail.com>
2026-07-27 20:50:03 +0530

    Added ternary program

---
+++ sample.java
@@ -0,0 +1,9 @@
+public class sample{
+    public static void main(String args[]){
+        int a =20;
+        int b=8;
+        if(a+b >= 28){
+            System.out.println("a + b is 28" );
+        }
+    }
+}
---
+++ simpleif.java
@@ -0,0 +1,7 @@
+public class simpleif{
+    public static void main(String args[]){
+        int a=10;
+        if(a>9)
+        System.out.println("a is ="+a);
+    }
+}
