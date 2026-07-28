807b45a387e9e1ebf56ae3c65d93bb668c9d4995
Meghana <meghanaks2006@gmail.com>
2026-07-28 20:38:21 +0530

    Added nestedif program

---
+++ nestedif.java
@@ -0,0 +1,16 @@
+public class nestedif{
+    public static void main (String args[]){
+        int a = 10;
+        int b = 20;
+        int c = 30;
+        if(a > b){
+            if(a + b  == 30)
+            System.out.println("a+b is 30");
+            else if(b + c == 40)
+                System.out.println("b+c is 50");
+        }
+            else 
+                System.out.println("a is not grater than b");
+        }
+            }
+        
