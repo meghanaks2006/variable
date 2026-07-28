ec7e32fb0acfac5fe21de2e208df41d06deff302
Meghana <meghanaks2006@gmail.com>
2026-07-28 20:49:18 +0530

    Added ternary program

--- ternary.java
+++ ternary.java
@@ -4,10 +4,10 @@ public class ternary
     {
         int n = 5;
         int result = 0;
-        if (n%2==0)
-            result = 10;
-        else
-            result = 20;
+       result = n%2==0? 10:20;
+         //   result = 10;
+        
+           // result = 20;
         System.out.println(result);
         
     }
