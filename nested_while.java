65a8f5bc76527351722305bf010aaf6f8ed6d065
Meghana <meghanaks2006@gmail.com>
2026-07-30 20:22:39 +0530

    Added nested_loop program

---
+++ nested_while.java
@@ -0,0 +1,15 @@
+public class nested_while{
+    public static void main(String args[]){
+        int i=1;
+        while(i<=3){
+            System.out.println("hii");
+        int j=1;
+        while(j<=2){
+            System.out.println("megha");
+            j++; 
+        }
+        i++;
+    }
+    }
+}
+
--- while_loop.java
+++ while_loop.java
@@ -1,7 +1,7 @@
 public class while_loop{
     public static void main(String args[]){
         int i=1;
-       while(i<=5){
+       while(i<=3){
         System .out.println("hii"+i);
         i++;
        }
