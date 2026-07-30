d5f1b6fc88ba672159aa7ddaf18fcd54af055c3b
Meghana <meghanaks2006@gmail.com>
2026-07-30 20:42:28 +0530

    Added do_while program

---
+++ do_while.java
@@ -0,0 +1,9 @@
+public class do_while{
+    public static void main(String args[]){
+        int i=5;
+        do{
+            System.out.println("hii megha");
+            i++;
+        }while(i<=4);
+    }
+}
